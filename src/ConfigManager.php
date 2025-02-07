735cb904f659a8969f9c965cd8e0b9e20d125ee8
Aaron Klump <sourcecode@intheloftstudios.com>
2025-02-07 15:19:08 -0800

    Change config name.

--- src/ConfigManager.php
+++ src/ConfigManager.php
@@ -6,7 +6,6 @@ namespace AKlump\ChangeAudio;
 use AKlump\ChangeAudio\Cache\CacheManager;
 use RuntimeException;
 
-
 /**
  * Manages configuration for the application.
  *
@@ -46,6 +45,10 @@ class ConfigManager {
   }
 
   public function path(): string {
+    if (empty($_SERVER['HOME']) || !file_exists($_SERVER['HOME']) || !is_dir($_SERVER['HOME'])) {
+      throw new RuntimeException('Missing $_SERVER[HOME]');
+    }
+
     return $_SERVER['HOME'] . '/.chaud.json';
   }
 
@@ -53,11 +56,6 @@ class ConfigManager {
     $default_config_path = __DIR__ . '/../install/config.json';
     if (!file_exists($default_config_path)) {
       throw new RuntimeException(sprintf('Missing default config: %s', $default_config_path));
-    }
-
-    $parent = dirname($config_path);
-    if (!file_exists($parent)) {
-      mkdir($parent, 0755, TRUE);
     }
 
     return copy($default_config_path, $config_path);
