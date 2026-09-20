638c19d3d1a8902031196af38bf94059486322ee
Aaron Klump <sourcecode@intheloftstudios.com>
2026-09-20 12:01:37 -0700

    Bumped version number to 0.0.12

--- src/App.php
+++ src/App.php
@@ -14,7 +14,7 @@ class App {
   /**
    * Keep in step with .web_package/config.yml when releasing.
    */
-  const VERSION = '0.0.11';
+  const VERSION = '0.0.12';
 
   /**
    * The leaf directory name inside the system temp directory.
